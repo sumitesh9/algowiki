66fa520589bda70444f0f1c81dcb884039d3e6da
Sumitesh Naithani <sumnaith@publicisgroupe.net>
2024-12-25 14:12:33 +0530

    feat: added recursion and backtracking

--- algowiki/next.config.mjs
+++ algowiki/next.config.mjs
@@ -1,15 +1,15 @@
-import remarkMath from 'remark-math'
-import rehypeKatex from 'rehype-katex'
-import nextra from 'nextra'
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import nextra from "nextra";
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
   latex: true,
   mdxOptions: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
-  }
-})
+  },
+});
 
-export default withNextra()
+export default withNextra();
